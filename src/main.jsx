90570bc5e9fab01730cad5396c3123d991b55e5d
itsannhienjoy <dophuongannhien3105@gmail.com>
2024-12-12 23:37:34 +1100

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+/**
+ * Node modules
+ */
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+/**
+ * Components
+ */
+import App from './App.jsx'
+
+/**
+ * CSS links
+ */
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
